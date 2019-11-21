2fa4d7aefa9564c8a324101852a9dfd39508fa50
yangke250 <yangke250@DESKTOP-AER1DMF>
2019-11-21 13:53:49 +0800

    增加清楚已读消息协议

--- easyim-biz-api/src/main/java/com/easyim/biz/api/protocol/c2s/ReadMessagePush.java
+++ easyim-biz-api/src/main/java/com/easyim/biz/api/protocol/c2s/ReadMessagePush.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class ReadMessagePush extends AbstractProtocol {
 	private static final long serialVersionUID = 1133349500935881112L;
-	private String toId;
 	private long cid;
 	private long lastMsgId;
 }
