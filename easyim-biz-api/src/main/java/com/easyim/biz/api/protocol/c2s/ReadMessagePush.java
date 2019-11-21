6418657e9d0c9830cae14883d261667c19b75803
yangke250 <yangke250@DESKTOP-AER1DMF>
2019-11-21 11:13:48 +0800

    修改已读消息协议

--- easyim-biz-api/src/main/java/com/easyim/biz/api/protocol/c2s/ReadMessagePush.java
+++ easyim-biz-api/src/main/java/com/easyim/biz/api/protocol/c2s/ReadMessagePush.java
@@ -7,4 +7,5 @@ public class ReadMessagePush extends AbstractProtocol {
 	private static final long serialVersionUID = 1133349500935881112L;
 	private String toId;
 	private long cid;
+	private long lastMsgId;
 }
